cea38dee4104d35a586cc51d2c31500b2f6d58d8
appbenigno <appbenigno@live.com.ph>
2016-08-17 21:12:06 +0800

    transmutation table updated

--- Grading System/Transmutation.Designer.cs
+++ Grading System/Transmutation.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lvGPA = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // lvGPA
+            // 
+            this.lvGPA.Location = new System.Drawing.Point(0, -1);
+            this.lvGPA.Name = "lvGPA";
+            this.lvGPA.Size = new System.Drawing.Size(508, 260);
+            this.lvGPA.TabIndex = 1;
+            this.lvGPA.UseCompatibleStateImageBehavior = false;
+            this.lvGPA.View = System.Windows.Forms.View.Details;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Letters",
+            "Uno is Gold",
+            "4 is Gold"});
+            this.comboBox1.Location = new System.Drawing.Point(194, 294);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(126, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Transmutation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(511, 464);
+            this.ClientSize = new System.Drawing.Size(508, 379);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lvGPA);
+            this.MaximizeBox = false;
             this.Name = "Transmutation";
             this.Text = "Transmutation Table";
             this.ResumeLayout(false);
@@ -42,6 +69,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvGPA;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Grading System/Transmutation.cs
+++ Grading System/Transmutation.cs
@@ -15,5 +15,279 @@ namespace Grading_System
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Letters")
+            {
+                lvGPA.Clear();
+
+                lvGPA.View = View.Details;
+                lvGPA.GridLines = true;
+                lvGPA.FullRowSelect = true;
+
+                lvGPA.Columns.Add("Grade", 100);
+                lvGPA.Columns.Add("Range From", 100);
+                lvGPA.Columns.Add("Range To", 100);
+                
+
+                //Add items in the listview
+                string[] arr = new string[4];
+                ListViewItem itm;
+
+                //Add first item
+                arr[0] = "A+";
+                arr[1] = "97";
+                arr[2] = "100";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "A";
+                arr[1] = "93";
+                arr[2] = "97";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "A-";
+                arr[1] = "90";
+                arr[2] = "92";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "B+";
+                arr[1] = "87";
+                arr[2] = "89";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "B";
+                arr[1] = "83";
+                arr[2] = "86";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "B-";
+                arr[1] = "80";
+                arr[2] = "82";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "C+";
+                arr[1] = "77";
+                arr[2] = "79";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "C";
+                arr[1] = "73";
+                arr[2] = "76";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "C-";
+                arr[1] = "70";
+                arr[2] = "72";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "D+";
+                arr[1] = "67";
+                arr[2] = "69";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "D";
+                arr[1] = "63";
+                arr[2] = "66";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "D-";
+                arr[1] = "60";
+                arr[2] = "62";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "F";
+                arr[1] = "0";
+                arr[2] = "59";                
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+            }
+
+
+            if (comboBox1.Text == "4 is Gold")
+            {
+                lvGPA.Clear();
+                lvGPA.LabelEdit = true;
+
+                lvGPA.View = View.Details;
+                lvGPA.GridLines = true;
+                lvGPA.FullRowSelect = true;
+
+                lvGPA.Columns.Add("Grade", 100);
+                lvGPA.Columns.Add("Range From", 100);
+                lvGPA.Columns.Add("Range To", 100);
+
+
+                //Add items in the listview
+                string[] arr = new string[4];
+                ListViewItem itm;
+
+                //Add first item
+                arr[0] = "4.0";
+                arr[1] = "97";
+                arr[2] = "100";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "4.0";
+                arr[1] = "93";
+                arr[2] = "97";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "3.7";
+                arr[1] = "90";
+                arr[2] = "92";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "3.3";
+                arr[1] = "87";
+                arr[2] = "89";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "3.0";
+                arr[1] = "83";
+                arr[2] = "86";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "2.7";
+                arr[1] = "80";
+                arr[2] = "82";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "2.3";
+                arr[1] = "77";
+                arr[2] = "79";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "2.0";
+                arr[1] = "73";
+                arr[2] = "76";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "1.7";
+                arr[1] = "70";
+                arr[2] = "72";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "1.3";
+                arr[1] = "67";
+                arr[2] = "69";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "1.0";
+                arr[1] = "63";
+                arr[2] = "66";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "0.7";
+                arr[1] = "60";
+                arr[2] = "62";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "0";
+                arr[1] = "0";
+                arr[2] = "59";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+            }
+
+
+            if (comboBox1.Text == "UNO is Gold")
+            {
+                lvGPA.Clear();
+                lvGPA.LabelEdit = true;
+
+                lvGPA.View = View.Details;
+                lvGPA.GridLines = true;
+                lvGPA.FullRowSelect = true;
+
+                lvGPA.Columns.Add("Grade", 100);
+                lvGPA.Columns.Add("Range From", 100);
+                lvGPA.Columns.Add("Range To", 100);
+
+
+                //Add items in the listview
+                string[] arr = new string[4];
+                ListViewItem itm;
+
+                //Add first item
+                arr[0] = "4.0";
+                arr[1] = "97";
+                arr[2] = "100";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "4.0";
+                arr[1] = "93";
+                arr[2] = "97";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "3.7";
+                arr[1] = "90";
+                arr[2] = "92";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "3.3";
+                arr[1] = "87";
+                arr[2] = "89";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "3.0";
+                arr[1] = "83";
+                arr[2] = "86";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "2.7";
+                arr[1] = "80";
+                arr[2] = "82";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "2.3";
+                arr[1] = "77";
+                arr[2] = "79";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "2.0";
+                arr[1] = "73";
+                arr[2] = "76";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "1.7";
+                arr[1] = "70";
+                arr[2] = "72";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "1.3";
+                arr[1] = "67";
+                arr[2] = "69";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "1.0";
+                arr[1] = "63";
+                arr[2] = "66";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+                arr[0] = "0.7";
+                arr[1] = "60";
+                arr[2] = "62";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+
+                arr[0] = "0";
+                arr[1] = "0";
+                arr[2] = "59";
+                itm = new ListViewItem(arr);
+                lvGPA.Items.Add(itm);
+            }
+        }
     }
 }
