50727531c441a0bea49b6cb091187b2b1b543d4c
appbenigno <appbenigno@live.com.ph>
2016-09-12 13:50:58 +0800

    loader files

---
+++ Grading%20System/Loader.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Grading_System
+{
+    partial class Loader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Skywalker, Luke _.",
+            "Jedi Knights",
+            "Quiz",
+            "Introduction to Science and Technology",
+            "150",
+            "150"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Skywalker, Luke _.",
+            "Jedi Knights",
+            "Quiz",
+            "Introduction to Mathematics",
+            "125",
+            "150"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Skywalker, Luke _.",
+            "Jedi Knights",
+            "Quiz",
+            "Introduction to Spelling",
+            "115",
+            "150"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Skywalker, Luke _.",
+            "Jedi Knights",
+            "Quiz",
+            "Introduction to Cloud Computing",
+            "150",
+            "150"}, -1);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lvEntries = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Grading_System.ProjResource.lbl_entries;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 28);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lvEntries
+            // 
+            this.lvEntries.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader10,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvEntries.FullRowSelect = true;
+            this.lvEntries.HideSelection = false;
+            this.lvEntries.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.lvEntries.Location = new System.Drawing.Point(13, 46);
+            this.lvEntries.Margin = new System.Windows.Forms.Padding(4);
+            this.lvEntries.Name = "lvEntries";
+            this.lvEntries.Size = new System.Drawing.Size(980, 193);
+            this.lvEntries.TabIndex = 13;
+            this.lvEntries.UseCompatibleStateImageBehavior = false;
+            this.lvEntries.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 196;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Section";
+            this.columnHeader10.Width = 165;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Activity Type";
+            this.columnHeader2.Width = 124;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Description";
+            this.columnHeader3.Width = 311;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Score";
+            this.columnHeader4.Width = 85;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Max Score";
+            this.columnHeader5.Width = 83;
+            // 
+            // Loader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lvEntries);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Loader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Loader_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView lvEntries;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
